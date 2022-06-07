9edbca4387b61f0ce0f5468b2ffc0b627ec03998
Ryan Punwasi <ryan.punwasi@gmail.com>
2022-06-07 14:30:49 -0400

    Handled edge cases

--- timer1.js
+++ timer1.js
@@ -1,6 +1,11 @@
 let args = process.argv.slice(2);
 let time = 0;
 
+args = args.filter((e) => {
+  return e > 0 && Number.isInteger(Number(e));
+});
+args.sort((a, b) => a - b);
+
 args.forEach(e => {
   time = e * 1000;
   setTimeout(() => {
